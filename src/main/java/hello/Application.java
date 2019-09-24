ecb6d2bd642a922b5a6566f396c7edeb3672a035
indreks <indrek.siilaberg@icefire.ee>
2019-09-24 09:40:38 +0300

    New branch

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,17 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    //private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    //static String s = "Testsds";
 
     public static void main(String[] args) {
+        //logger.info("Prindin välja %s", s);
         SpringApplication.run(Application.class, args);
     }
 
